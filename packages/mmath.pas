40c2935a3006af71fce36c8084a60811e5359d2b
Patryk Wychowaniec <patryk432@gmail.com>
2013-07-28 14:26:35 +0200

    More math stdlib functions.

--- packages/mmath.pas
+++ packages/mmath.pas
@@ -13,56 +13,131 @@ Unit mMath;
  Implementation
 Uses Math;
 
-{ math.sqrt }
-Procedure _sqrt(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
+{ math.cos }
+Procedure _cos(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
 Begin
- Result^.Typ         := mvFloat;
- Result^.Value.Float := sqrt(getFloat(Params[0]));
+ Result^ := cos(getFloat(Params[0]));
 End;
 
 { math.sin }
 Procedure _sin(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
 Begin
- Result^.Typ         := mvFloat;
- Result^.Value.Float := sin(getFloat(Params[0]));
+ Result^ := sin(getFloat(Params[0]));
 End;
 
-{ math.cos }
-Procedure _cos(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
+{ math.tan }
+Procedure _tan(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
 Begin
- Result^.Typ         := mvFloat;
- Result^.Value.Float := cos(getFloat(Params[0]));
+ Result^ := tan(getFloat(Params[0]));
 End;
 
-{ math.log }
-Procedure _log(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
+{ math.acos }
+Procedure _acos(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
 Begin
- Result^.Typ         := mvFloat;
- Result^.Value.Float := logn(getFloat(Params[0]), getFloat(Params[1]));
+ Result^ := arccos(getFloat(Params[0]));
 End;
 
-{ math.ln }
-Procedure _ln(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
+{ math.asin }
+Procedure _asin(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
 Begin
- Result^.Typ         := mvFloat;
- Result^.Value.Float := ln(getFloat(Params[0]));
+ Result^ := arcsin(getFloat(Params[0]));
+End;
+
+{ math.atan }
+Procedure _atan(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
+Begin
+ Result^ := arctan(getFloat(Params[0]));
+End;
+
+{ math.atan2 }
+Procedure _atan2(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
+Begin
+ Result^ := arctan2(getFloat(Params[0]), getFloat(Params[1]));
+End;
+
+{ math.cosh }
+Procedure _cosh(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
+Begin
+ Result^ := cosh(getFloat(Params[0]));
+End;
+
+{ math.sinh }
+Procedure _sinh(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
+Begin
+ Result^ := sinh(getFloat(Params[0]));
+End;
+
+{ math.tanh }
+Procedure _tanh(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
+Begin
+ Result^ := tanh(getFloat(Params[0]));
+End;
+
+{ math.acosh }
+Procedure _acosh(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
+Begin
+ Result^ := arccosh(getFloat(Params[0]));
+End;
+
+{ math.asinh }
+Procedure _asinh(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
+Begin
+ Result^ := arcsinh(getFloat(Params[0]));
+End;
+
+{ math.atanh }
+Procedure _atanh(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
+Begin
+ Result^ := arctanh(getFloat(Params[0]));
 End;
 
 { math.exp }
 Procedure _exp(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
 Begin
- Result^.Typ         := mvFloat;
- Result^.Value.Float := exp(getFloat(Params[0]));
+ Result^ := exp(getFloat(Params[0]));
+End;
+
+{ math.log }
+Procedure _log(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
+Begin
+ Result^ := logn(getFloat(Params[0]), getFloat(Params[1]));
+End;
+
+{ math.ln }
+Procedure _ln(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
+Begin
+ Result^ := ln(getFloat(Params[0]));
+End;
+
+{ math.sqrt }
+Procedure _sqrt(VM: Pointer; Params: PMixedValue; Result: PMixedValue);
+Begin
+ Result^ := sqrt(getFloat(Params[0]));
 End;
 
 // -------------------------------------------------------------------------- //
 Procedure Init(VM: Pointer);
 Begin
- AddInternalCall(VM, 'math', 'sqrt', 1, @_sqrt);
- AddInternalCall(VM, 'math', 'sin', 1, @_sin);
  AddInternalCall(VM, 'math', 'cos', 1, @_cos);
+ AddInternalCall(VM, 'math', 'sin', 1, @_sin);
+ AddInternalCall(VM, 'math', 'tan', 1, @_tan);
+
+ AddInternalCall(VM, 'math', 'acos', 1, @_acos);
+ AddInternalCall(VM, 'math', 'asin', 1, @_asin);
+ AddInternalCall(VM, 'math', 'atan', 1, @_atan);
+ AddInternalCall(VM, 'math', 'atan2', 2, @_atan2);
+
+ AddInternalCall(VM, 'math', 'cosh', 1, @_cosh);
+ AddInternalCall(VM, 'math', 'sinh', 1, @_sinh);
+ AddInternalCall(VM, 'math', 'tanh', 1, @_tanh);
+
+ AddInternalCall(VM, 'math', 'acosh', 1, @_acosh);
+ AddInternalCall(VM, 'math', 'asinh', 1, @_asinh);
+ AddInternalCall(VM, 'math', 'atanh', 1, @_atanh);
+
+ AddInternalCall(VM, 'math', 'exp', 1, @_exp);
  AddInternalCall(VM, 'math', 'log', 2, @_log);
  AddInternalCall(VM, 'math', 'ln', 1, @_ln);
- AddInternalCall(VM, 'math', 'exp', 1, @_exp);
+ AddInternalCall(VM, 'math', 'sqrt', 1, @_sqrt);
 End;
 End.
